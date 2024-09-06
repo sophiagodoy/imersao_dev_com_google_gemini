6cc6324af202cd8418b2bb3e3d26a4356b73f6b3
Sophia Franco de Godoy <161141869+sophiagodoy@users.noreply.github.com>
2024-09-06 14:39:26 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,23 @@
+// ARQUIVO PARA DADOS 
+
+// CRIANDO VARIÁVEIS - LISTAS E OBJETOS 
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e se tornou um ícone para a nova geração de skatistas",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua força e técnica impressionaram o mundo do judô.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza#:~:text=Beatriz%20Souza%20(Itariri,%2020%20de%20maio%20de%201998)%20%C3%A9%20uma", 
+        tags: "judo judô judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que também brilhou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro no individual geral. Sua versatilidade e elegância a tornaram uma das principais ginastas do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
